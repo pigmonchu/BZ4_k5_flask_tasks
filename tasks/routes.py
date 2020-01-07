9ff16cc8937b13d55ad4a45f77808c29016a9bb4
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-01-07 22:30:24 +0100

    Skeleton

--- tasks/routes.py
+++ tasks/routes.py
@@ -1,15 +1,33 @@
 from tasks import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 import csv
 
+DATOS = './data/tareas.txt'
+cabecera = ['title', 'description', 'date']
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
+    fdatos = open(DATOS, 'r')
+    csvreader = csv.reader(fdatos, delimiter=",", quotechar='"')
+
+    registros = []
+    for linea in csvreader:
+        registros.append(linea)
+
+    fdatos.close()
+    print()
+    print(registros)
+    print()
+    return render_template("index.html", registros=registros) 
+
+
+@app.route("/newtask", methods=['GET', 'POST'])
+def newTask():
     if request.method == 'GET':
         return render_template("task.html")
 
-    fdatos = open('./data/tareas.dat', 'w')
+    fdatos = open(DATOS, 'a')
     csvwriter = csv.writer(fdatos, delimiter=",", quotechar='"')
 
     title = request.values.get('title')
@@ -19,4 +37,4 @@ def index():
     csvwriter.writerow([title, desc, date])
 
     fdatos.close()
-    return render_template("task.html")
+    return redirect(url_for("index"))
