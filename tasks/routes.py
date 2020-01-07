8c64539574387e1cb759ebf13f17f67c478dfcb8
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-01-07 16:13:17 +0100

    Añadir fila

--- tasks/routes.py
+++ tasks/routes.py
@@ -1,22 +1,56 @@
 from tasks import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 import csv
 
+DATOS = './data/tareas.dat'
+header = ['title', 'description', 'date']
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template("task.html")
+def todas():
+    fdatos = open(DATOS, 'r')
+    csvreader = csv.reader(fdatos, delimiter=",", quotechar='"')
 
-    fdatos = open('./data/tareas.dat', 'w')
+    registros = []
+    for linea in csvreader:
+        registros.append(linea)
+        print(linea)
+
+    filas = []
+    for datos in registros:
+        d = {}
+
+        for ix, nombre_campo in enumerate(header): 
+            d[nombre_campo] = datos[ix]
+        filas.append(d)
+
+    fdatos.close()
+
+    return filas
+
+def append(task):
+    fdatos = open(DATOS, 'a')
     csvwriter = csv.writer(fdatos, delimiter=",", quotechar='"')
 
-    title = request.values.get('title')
-    desc = request.values.get('desc')
-    date = request.values.get('date')
+    title = task.get('title')
+    desc = task.get('description')
+    date = task.get('date')
 
     csvwriter.writerow([title, desc, date])
 
     fdatos.close()
-    return render_template("task.html")
+
+
+
+@app.route("/")
+def index():
+    filas = todas()
+    return render_template("index.html", tareas=filas)
+
+@app.route("/newtask", methods=['GET', 'POST'])
+def newTask():
+    if request.method == 'GET':
+        return render_template("task.html")
+
+    append(request.values)
+
+    return redirect(url_for("index"))
