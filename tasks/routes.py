8640da6aaabad1844527bcb47f1e858bb24fb79b
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-01-10 23:36:41 +0100

    CRUD con sqlite

--- tasks/routes.py
+++ tasks/routes.py
@@ -24,6 +24,65 @@ def renameFiles(DATOS, COPIA):
     os.remove(DATOS)
     os.rename(COPIA, DATOS)
 
+def todasTareasDB():
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor =conn.cursor()
+    consulta = 'SELECT titulo, descripcion, fecha, id FROM tareas;'
+    rows = cursor.execute(consulta)
+    filas = []
+    for row in rows:
+        filas.append(row)
+
+    print(filas)
+    conn.close()
+    return filas
+
+def addTaskDB(title, description, fx):
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    consulta = """
+        INSERT INTO tareas (titulo, descripcion, fecha)
+                    VALUES (?, ?, ?);
+    """
+    cursor.execute(consulta, (title, description, fx))
+    conn.commit()
+    conn.close()
+
+def leeTaskDB(id):
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    consulta = """
+        SELECT titulo, descripcion, fecha, id FROM tareas
+         WHERE id = ?;
+    """
+    rows = cursor.execute(consulta, (id,))
+    fila = rows.fetchone()
+
+    conn.close()
+    return fila
+
+def borraTaskDB(id):
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    consulta = """
+        DELETE FROM tareas
+         WHERE id = ?;
+    """
+    cursor.execute(consulta, (id,))
+    conn.commit()
+    conn.close()
+
+def modTaskDB(id):
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    consulta = """
+        UPDATE tareas
+           SET titulo = ?, descripcion = ?, fecha = ?
+         WHERE id = ?;
+    """
+    cursor.execute(consulta, (request.values.get('title'), request.values.get('description'), request.values.get('fx'), id))
+    conn.commit()
+    conn.close()
 
 def todasTareas():
     fdatos = open(DATOS, 'r')
@@ -82,9 +141,8 @@ def modTask(ix, borra=False):
 
 @app.route("/")
 def index():
-    registros = todasTareas()
+    registros = todasTareasDB()
     return render_template("index.html", registros=registros) 
-
 
 @app.route("/newtask", methods=['GET', 'POST'])
 def newTask():
@@ -98,7 +156,7 @@ def newTask():
         desc = request.values.get('description')
         fx = request.values.get('fx')
 
-        addTask(title, desc, fx)
+        addTaskDB(title, desc, fx)
 
         return redirect(url_for("index"))
     else:
@@ -111,7 +169,7 @@ def proccesTask():
     if request.method == 'GET':
         ix = request.values.get('ix')
         if ix:
-            registroAct = leeTask(ix)
+            registroAct = leeTaskDB(ix)
 
             if registroAct:
                 if registroAct[2]:
@@ -136,14 +194,14 @@ def proccesTask():
 
     if form.btn.data == 'B':
         ix = int(request.values.get('ix'))
-        borraTask(ix)
+        borraTaskDB(ix)
 
         return redirect(url_for('index'))
     
     if form.btn.data == 'M':
         if form.validate():
             ix = int(request.values.get('ix'))
-            modTask(ix)
+            modTaskDB(ix)
             return redirect(url_for("index"))
         return render_template("processtask.html", form=form)
 
