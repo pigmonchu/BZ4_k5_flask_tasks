8c519422e71a9fd38f7752ee04407db676637920
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-01-11 00:37:50 +0100

    refactorizando sqlite

--- tasks/routes.py
+++ tasks/routes.py
@@ -2,146 +2,42 @@ from tasks import app
 from flask import render_template, request, redirect, url_for
 from tasks.forms import TaskForm, ProccesTaskForm
 
-import csv, sqlite3, os
+import sqlite3
 from datetime import date
 
-DATOS = './data/tareas.txt'
-COPIA = './data/copia.txt'
 BASE_DATOS = './data/tasks.db'
 
-cabecera = ['title', 'description', 'date']
+def dict_factory(cursor, row):
+    d = {}
+    for ix, col in enumerate(cursor.description):
+        d[col[0]] = row[ix]
+    return d
 
-def openFiles(DATOS, COPIA):
-    original = open(DATOS, 'r')
-    copia = open(COPIA, 'w')
-    return original, copia
-
-def closeFiles(original, copia):
-    original.close()
-    copia.close()
-
-def renameFiles(DATOS, COPIA):
-    os.remove(DATOS)
-    os.rename(COPIA, DATOS)
-
-def todasTareasDB():
+def dbQuery(consulta, *args):
     conn = sqlite3.connect(BASE_DATOS)
-    cursor =conn.cursor()
-    consulta = 'SELECT titulo, descripcion, fecha, id FROM tareas;'
-    rows = cursor.execute(consulta)
-    filas = []
-    for row in rows:
-        filas.append(row)
+    conn.row_factory = dict_factory
 
-    print(filas)
-    conn.close()
-    return filas
-
-def addTaskDB(title, description, fx):
-    conn = sqlite3.connect(BASE_DATOS)
     cursor = conn.cursor()
-    consulta = """
-        INSERT INTO tareas (titulo, descripcion, fecha)
-                    VALUES (?, ?, ?);
-    """
-    cursor.execute(consulta, (title, description, fx))
+
+    rows = cursor.execute(consulta, args).fetchall()
+
+    if len(rows) == 1:
+        rows = rows[0]
+    elif len(rows) == 0:
+        rows = None
+
     conn.commit()
     conn.close()
 
-def leeTaskDB(id):
-    conn = sqlite3.connect(BASE_DATOS)
-    cursor = conn.cursor()
-    consulta = """
-        SELECT titulo, descripcion, fecha, id FROM tareas
-         WHERE id = ?;
-    """
-    rows = cursor.execute(consulta, (id,))
-    fila = rows.fetchone()
-
-    conn.close()
-    return fila
-
-def borraTaskDB(id):
-    conn = sqlite3.connect(BASE_DATOS)
-    cursor = conn.cursor()
-    consulta = """
-        DELETE FROM tareas
-         WHERE id = ?;
-    """
-    cursor.execute(consulta, (id,))
-    conn.commit()
-    conn.close()
-
-def modTaskDB(id):
-    conn = sqlite3.connect(BASE_DATOS)
-    cursor = conn.cursor()
-    consulta = """
-        UPDATE tareas
-           SET titulo = ?, descripcion = ?, fecha = ?
-         WHERE id = ?;
-    """
-    cursor.execute(consulta, (request.values.get('title'), request.values.get('description'), request.values.get('fx'), id))
-    conn.commit()
-    conn.close()
-
-def todasTareas():
-    fdatos = open(DATOS, 'r')
-    csvreader = csv.reader(fdatos, delimiter=",", quotechar='"')
-
-    registros = []
-    for linea in csvreader:
-        registros.append(linea)
-
-    fdatos.close()
-    return registros
-
-def addTask(title, desc, fx):
-    fdatos = open(DATOS, 'a')
-    csvwriter = csv.writer(fdatos, delimiter=",", quotechar='"')
-    csvwriter.writerow([title, desc, fx])
-    fdatos.close()
-
-def leeTask(ix):
-    fdatos = open(DATOS, 'r')
-    csvreader = csv.reader(fdatos, delimiter=",", quotechar='"')
-
-    registroAct = None
-
-    ix = int(ix)
-    for ilinea, linea in enumerate(csvreader, start=1):
-        if ilinea == ix:
-            registroAct = linea
-            break
-
-    return registroAct
-
-def borraTask(ix):
-    return modTask(ix, True)
-
-def modTask(ix, borra=False):
-    original, copia = openFiles(DATOS, COPIA)
-    csvreader = csv.reader(original, delimiter=",", quotechar='"')
-    for ilinea, linea in enumerate(csvreader, start=1):
-        csvwriter = csv.writer(copia, delimiter=",", quotechar='"', lineterminator='\r')
-    
-        if ilinea == ix:
-            if not borra:
-                title = request.values.get('title')
-                desc = request.values.get('description')
-                fx = request.values.get('fx')
-                csvwriter.writerow([title, desc, fx])
-        else:
-            title = linea[0]
-            desc = linea[1]
-            fx = linea[2]
-            csvwriter.writerow([title, desc, fx])
-    closeFiles(original, copia)
-    renameFiles(DATOS, COPIA)
-
+    return rows
 
 @app.route("/")
 def index():
-    registros = todasTareasDB()
+    registros = dbQuery('SELECT titulo, descripcion, fecha, id FROM tareas;')
+    
+    if isinstance(registros, tuple):
+        registros = [registros]
+
     return render_template("index.html", registros=registros) 
 
 @app.route("/newtask", methods=['GET', 'POST'])
@@ -156,7 +52,11 @@ def newTask():
         desc = request.values.get('description')
         fx = request.values.get('fx')
 
-        addTaskDB(title, desc, fx)
+        consulta = """
+        INSERT INTO tareas (titulo, descripcion, fecha)
+                    VALUES (?, ?, ?);
+        """
+        dbQuery(consulta, title, desc, fx)
 
         return redirect(url_for("index"))
     else:
@@ -169,11 +69,11 @@ def proccesTask():
     if request.method == 'GET':
         ix = request.values.get('ix')
         if ix:
-            registroAct = leeTaskDB(ix)
+            registroAct = dbQuery('Select titulo, descripcion, fecha, id from tareas where id = ?;', ix)
 
             if registroAct:
-                if registroAct[2]:
-                    fechaTarea = date(int(registroAct[2][:4]), int(registroAct[2][5:7]), int(registroAct[2][8:]))
+                if registroAct['fecha']:
+                    fechaTarea = date(int(registroAct['fecha'][:4]), int(registroAct['fecha'][5:7]), int(registroAct['fecha'][8:]))
                 else:
                     fechaTarea = None
 
@@ -186,7 +86,7 @@ def proccesTask():
                     accion = 'B'
 
 
-                form = ProccesTaskForm(data={'ix': ix, 'title': registroAct[0], 'description': registroAct[1], 'fx': fechaTarea, 'btn': accion})
+                form = ProccesTaskForm(data={'ix': ix, 'title': registroAct['titulo'], 'description': registroAct['descripcion'], 'fx': fechaTarea, 'btn': accion})
 
             return render_template("processtask.html", form=form)
         else:
@@ -194,14 +94,28 @@ def proccesTask():
 
     if form.btn.data == 'B':
         ix = int(request.values.get('ix'))
-        borraTaskDB(ix)
+        consulta = """
+            DELETE FROM tareas
+                WHERE id = ?;
+        """
+        dbQuery(consulta, ix)
 
         return redirect(url_for('index'))
     
     if form.btn.data == 'M':
         if form.validate():
             ix = int(request.values.get('ix'))
-            modTaskDB(ix)
+            consulta = """
+                UPDATE tareas
+                SET titulo = ?, descripcion = ?, fecha = ?
+                WHERE id = ?;
+            """
+            dbQuery(consulta, 
+                    request.values.get('title'), 
+                    request.values.get('description'), 
+                    request.values.get('fx'), 
+                    ix)
+
             return redirect(url_for("index"))
         return render_template("processtask.html", form=form)
 
