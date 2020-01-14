722282babd15b3ba1fa8aad980763e0a30914e58
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-01-14 23:19:45 +0100

    Corrección de error

--- tasks/routes.py
+++ tasks/routes.py
@@ -35,10 +35,14 @@ def dbQuery(consulta, *args):
 def index():
     registros = dbQuery('SELECT titulo, descripcion, fecha, id FROM tareas;')
     
-    if isinstance(registros, tuple):
-        registros = [registros]
+    if registros:
+        if isinstance(registros, dict):
+            registros = [registros]
+    else:
+        registros = []
 
     return render_template("index.html", registros=registros) 
+    
 
 @app.route("/newtask", methods=['GET', 'POST'])
 def newTask():
