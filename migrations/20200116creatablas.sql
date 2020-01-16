f7f3e39003bf6ed0c598650441b8c86cd061b833
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-01-16 21:18:50 +0100

    y las migraciones

---
+++ migrations/20200116creatablas.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS empleados;
+
+CREATE TABLE "empleados" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"name"	TEXT NOT NULL,
+	"apellidos"	TEXT NOT NULL,
+	"email"	TEXT NOT NULL
+);
